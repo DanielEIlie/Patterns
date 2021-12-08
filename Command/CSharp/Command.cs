23c61f992e0ccb88a6749468b8acda369544f788
Daniel Ilie <daniel.ilie@woodplc.com>
2021-12-08 12:50:41 +0000

    Implemented abstract class Command

--- Command/CSharp/Command.cs
+++ Command/CSharp/Command.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace CSharp
 {
-  class Command
+  abstract class Command
   {
+    private Application _app;
+    private Editor _editor;
+    private string _backup;
+
+    public Command(Application app, Editor editor)
+    {
+      _app = app;
+      _editor = editor;
+    }
+
+    /// <summary>
+    /// Makes a backup of the editor's state.
+    /// </summary>
+    public void SaveBackup()
+    {
+      _backup = _editor.Text;
+    }
+
+    /// <summary>
+    /// Restores the stored editor's state.
+    /// </summary>
+    public void Undo()
+    {
+      _editor.Text = _backup;
+    }
+
+    /// <summary>
+    /// Executes the command.
+    /// </summary>
+    /// <returns>True if the editor's state was changed, false otherwise.</returns>
+    public abstract bool Execute();
   }
 }
