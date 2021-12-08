51fa5c1ce09c43f9948ebef56d095f6b717790d2
Daniel Ilie <daniel.ilie@woodplc.com>
2021-12-08 11:40:25 +0000

    Created the Text Editor.

---
+++ Command/CSharp/Editor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+  class Editor
+  {
+    public string Text { get; set; }
+
+    public string GetSelection( int startIndex, int length )
+    {
+      if ( !string.IsNullOrEmpty( Text ) &&
+        length > 0 &&
+        startIndex + length - 1 < Text.Length )
+      {
+        return Text.Substring( startIndex, length );
+      }
+      return null;
+    }
+
+    public string DeleteSelection( int startIndex, int length )
+    {
+      return Text.Remove( startIndex, length );
+    }
+
+    public string DeleteSelection( string text )
+    {
+      return Text.Replace( text, "" ); // or ReplaceSelection( text, "" );
+    }
+
+    public string ReplaceSelection( string existingText, string newText )
+    {
+      return Text.Replace(existingText, newText);
+    }
+
+    public string InsertSelection( int startIndex, string text )
+    {
+      return Text.Insert( startIndex, text );
+    }
+  }
+}
---
+++ Command/CSharp/Testing.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+  internal class Testing
+  {
+    internal static void TestingEditor()
+    {
+      Editor editor = new();
+      editor.Text =
+        "Around 59,000 homes and businesses are without power\n" +
+        "as Storm Barra continues to track across\n" +
+        "the northwest and northern half of the country.\n";
+      Console.WriteLine( "Original String:" );
+      Console.WriteLine( editor.Text );
+
+      string selectedText = editor.GetSelection( 14, 20 );
+      Console.WriteLine( "Testing GetSelection:" );
+      Console.WriteLine( selectedText + "\n" );
+
+      editor.Text = editor.InsertSelection( 14, "schools, universities, " );
+      Console.WriteLine( "Testing InsertSelection:" );
+      Console.WriteLine( editor.Text );
+
+      editor.Text = editor.DeleteSelection( "schools, universities, " );
+      Console.WriteLine( "Testing DeleteSelection with text:" );
+      Console.WriteLine( editor.Text );
+
+      editor.Text = editor.ReplaceSelection( selectedText, "businesses and homes" );
+      Console.WriteLine( "Testing ReplaceSelection:" );
+      Console.WriteLine( editor.Text );
+
+      editor.Text = editor.DeleteSelection( 14, 21 );
+      Console.WriteLine( "Testing DeleteSelection with indices:" );
+      Console.WriteLine( editor.Text );
+    }
+  }
+}
