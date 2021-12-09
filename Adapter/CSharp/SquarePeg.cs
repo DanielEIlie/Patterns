da5a9e231099816399be48562ec11347b01281d0
Daniel Ilie <daniel.ilie@woodplc.com>
2021-12-09 10:44:44 +0000

    Looks more like an adapter.

--- Adapter/CSharp/SquarePeg.cs
+++ Adapter/CSharp/SquarePeg.cs
@@ -5,7 +5,7 @@ namespace CSharp
   class SquarePeg
   {
     public double Width { get; private set; }
-    public double Radius { get => Width / Math.Sqrt( 2.0 ); }
+    //public double Radius { get => Width / Math.Sqrt( 2.0 ); }
 
     public SquarePeg( double width )
     {
